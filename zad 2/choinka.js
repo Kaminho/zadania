69d15f88fdc9145ba0f943daad726a23fb7e0ec3
Kamil Wiszniewski <kamil.wiszniewski@billennium.com>
2019-02-21 14:06:24 +0100

    zad 2

--- zad 2/choinka.js
+++ zad 2/choinka.js
@@ -1,1 +1,26 @@
 console.log("choinka.js");
+const treeDiv = document.getElementById('tree');
+
+const inputs = document.getElementsByTagName('input');
+
+function createTree() {
+  treeDiv.innerHTML = '';
+  const lvls = inputs[0].value;
+  const capacity = inputs[1].value;
+  
+  let tree = '*</br>';
+  let lvl = 0;
+
+  for (let i = 1; i < lvls; i++) {
+    const reminder = i % capacity; 
+    if (reminder === 0) {
+      lvl += 2;
+    }
+    tree += '/' + ('&nbsp;'.repeat(i * 2 - lvl)) + '\\</br>';
+  }
+
+  const div = document.getElementsByClassName('actions')[0].getElementsByTagName('div')[0];
+  div.innerHTML = 'Liczba pięter: ' + Math.round(lvls / capacity).toString();
+
+  treeDiv.innerHTML = tree;
+}
